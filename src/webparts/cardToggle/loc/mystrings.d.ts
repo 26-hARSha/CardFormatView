099e9fe1bf16b618637b29ef2143c9e66204dbfe
26-hARSha <harshadasannake@outlook.com>
2023-06-07 00:05:26 +0530

    Demo 1

---
+++ src/webparts/cardToggle/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ICardToggleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'CardToggleWebPartStrings' {
+  const strings: ICardToggleWebPartStrings;
+  export = strings;
+}
